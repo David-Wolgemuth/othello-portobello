514910ec0754cdd2881c38c97d0d8962c770fdbe
Kelin Christi <christi.kelin20@gmail.com>
2016-04-03 18:01:01 -0700

    Added logout button in a custom popover view

---
+++ Othello/Othello/UserInfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  UserInfoViewController.swift
+//  Othello
+//
+//  Created by Kelin Christi on 4/3/16.
+//  Copyright © 2016 David. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserInfoViewController {
+    
+    
+}
